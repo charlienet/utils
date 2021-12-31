ebd297ce69e7ce1e913ee07fb0cfe6169ec486e0
charlie <3140647@qq.com>
2021-12-31 15:03:55 +0800

    json

---
+++ json/json.go
@@ -0,0 +1,14 @@
+//go:build !jsoniter
+// +build !jsoniter
+
+package json
+
+import "encoding/json"
+
+var (
+	Marshal       = json.Marshal
+	Unmarshal     = json.Unmarshal
+	MarshalIndent = json.MarshalIndent
+	NewDecoder    = json.NewDecoder
+	NewEncoder    = json.NewEncoder
+)
