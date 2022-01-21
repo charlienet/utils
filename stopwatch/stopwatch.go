792b3779364b9d4dfd9d2989b9484b7615c96a40
charlie <3140647@qq.com>
2022-01-21 11:35:44 +0800

    使用单调时间

--- stopwatch/stopwatch.go
+++ stopwatch/stopwatch.go
@@ -1,11 +1,13 @@
 package stopwatch
 
-import "time"
+import (
+	"time"
+)
 
 type watch struct {
-	elapsed        int64
-	startTimestamp int64
-	isRunning      bool
+	startTime time.Time
+	elapsed   time.Duration
+	isRunning bool
 }
 
 func StartNew() *watch {
@@ -17,7 +19,7 @@ func StartNew() *watch {
 
 func (w *watch) Start() {
 	if !w.isRunning {
-		w.startTimestamp = getTimestamp()
+		w.startTime = time.Now()
 		w.isRunning = true
 	}
 }
@@ -25,21 +27,20 @@ func (w *watch) Start() {
 // 将运行时间重置为零，并开始测量运行时间。
 func (w *watch) Restart() {
 	w.elapsed = 0
-	w.startTimestamp = getTimestamp()
+	w.startTime = time.Now()
 	w.isRunning = true
 }
 
 // 停止时间间隔测量并将经过的时间重置为零。
 func (w *watch) Reset() {
 	w.elapsed = 0
-	w.startTimestamp = 0
+	w.startTime = time.Now()
 	w.isRunning = false
 }
 
 func (w *watch) Stop() {
 	if w.isRunning {
-		timestamp := getTimestamp()
-		num := timestamp - w.startTimestamp
+		num := time.Since(w.startTime)
 		w.elapsed += num
 		w.isRunning = false
 
@@ -68,13 +69,8 @@ func (w *watch) ElapsedMicroseconds() int64 {
 func (w *watch) ElapsedNanoseconds() int64 {
 	num := w.elapsed
 	if w.isRunning {
-		timestamp := getTimestamp()
-		num += (timestamp - w.startTimestamp)
+		num += time.Since(w.startTime)
 	}
 
-	return num
-}
-
-func getTimestamp() int64 {
-	return time.Now().UnixNano()
+	return num.Nanoseconds()
 }
